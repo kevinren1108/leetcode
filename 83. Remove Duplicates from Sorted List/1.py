713b18137a1f8c46bfe781607b7c205620308bab
Kevin Ren <kevinren1108@gmail.com>
2022-02-17 23:01:55 -0600

    update

--- 83. Remove Duplicates from Sorted List/1.py
+++ 83. Remove Duplicates from Sorted List/1.py
@@ -1,27 +1,10 @@
 def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-    #variabbles:  preVal,currVal
-    #printers:    prev,curr
-    if not head: return head
-    if not head.next: return head
-    
-    prev = ListNode(-111,None)
     curr = head
     
-    preVal = prev.val
-    currVal = curr.val
-    
-    while curr.next:
-        if currVal == preVal:
-            prev.next = curr.next
-            
-            preVal = prev.val
-            currVal = curr.val
-            
-            prev = curr
-            curr = curr.next
-            
+    while curr:
+        if curr.next and curr.val == curr.next.val:
+            curr.next = curr.next.next
         else:
-            prev = curr
             curr = curr.next
             
-    return head       
+    return head
