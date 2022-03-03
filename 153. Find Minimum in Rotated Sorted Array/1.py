00e3d86b48ab6744ad5d0841a921f6c3c7f7d0d4
Kevin Ren <kevinren1108@gmail.com>
2022-03-03 17:41:33 -0600

    153

---
+++ 153. Find Minimum in Rotated Sorted Array/1.py
@@ -0,0 +1,14 @@
+def findMin(nums: List[int]) -> int:
+#         [3,4,5,1,2]
+#          l.  m.  r    
+#         we have l , r , mid 
+#         if r < mid, we know that the right potion is rotated. 
+#         so we shoud update our left pointer, and tring to find the result on the right hand side
+#         [6,7,1,2,3,4]
+#          l.    m.  r
+#         in the other case 
+#         we have r < mid and l > mid:
+#         Than we are sure that the target number is on the left hand side.
+#         then we update our right pointer and tring search the target on the left side
+        
+#         we should wait l >= r happen, at that time, the left pointer should point to the target
