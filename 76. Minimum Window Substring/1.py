648a571fb42b18c8cde5c88af8dc13a0e8aed8f6
Kevin Ren <kevinren1108@gmail.com>
2022-03-10 22:35:57 -0600

    update

--- 76. Minimum Window Substring/1.py
+++ 76. Minimum Window Substring/1.py
@@ -1,23 +1,61 @@
+from collections import Counter
+from math import inf
 
 
-import collections
+def minWindow(s: str, t: str) -> str:
+    if t == "" : return ""
+  
+    need = Counter(t)
+    have = {}
+    for n in t:
+        have[n] = 0
+
+    l,r = 0, 0
+    needCount, haveCount = len(need),0
+    ans = ""
 
 
-def minWindow( s, t):
-    need, missing = collections.Counter(t), len(t)
-    i = I = J = 0
-    for j, c in enumerate(s, 1):
-        missing -= need[c] > 0
-        need[c] -= 1
-        if not missing:
-            while i < j and need[s[i]] < 0:
-                need[s[i]] += 1
-                i += 1
-            if not J or j - i <= J - I:
-                I, J = i, j
-    return s[I:J]
+    size = float(inf)
+    while r < len(s):
+        if s[r] in need:
+            have[s[r]] += 1
+            if have[s[r]] == need[s[r]]:
+                haveCount += 1
+        r += 1
 
-s = "ADOBECODEBANC"
-t = "ABC"
+        while needCount == haveCount:
+            if s[l] in have:
+                have[s[l]] -= 1
+                if have[s[l]] < need[s[l]]:
+                    haveCount -= 1
+            if (r - l + 1) < size:
+                ans = s[l:r]
+                size = len(ans)
+            l += 1
 
-minWindow(s,t)
+
+    return ans 
+
+minWindow("cabwefgewcwaefgcf","cae")
+
+# import collections
+
+
+# def minWindow( s, t):
+#     need, missing = collections.Counter(t), len(t)
+#     i = I = J = 0
+#     for j, c in enumerate(s, 1):
+#         missing -= need[c] > 0
+#         need[c] -= 1
+#         if not missing:
+#             while i < j and need[s[i]] < 0:
+#                 need[s[i]] += 1
+#                 i += 1
+#             if not J or j - i <= J - I:
+#                 I, J = i, j
+#     return s[I:J]
+
+# s = "ADOBECODEBANC"
+# t = "ABC"
+
+# minWindow(s,t)
