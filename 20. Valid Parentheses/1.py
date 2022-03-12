56214ca4f0b6e91384b675b8f1171fecfbc52575
Kevin Ren <kevinren1108@gmail.com>
2022-03-11 20:34:01 -0600

    update

--- 20. Valid Parentheses/1.py
+++ 20. Valid Parentheses/1.py
@@ -1,12 +1,40 @@
+def isValid(self, s: str) -> bool:
+        
+    
+    stack = []
+    
+    for ch in s:
+        if ch == "(" or ch == "[" or ch == "{":
+            stack.append(ch)
+            
+        elif len(stack) != 0:
+            if ch == ")" or ch == "]" or ch == "}" :
+                curr = stack.pop()
 
-def isValid(s: str) -> bool:
-    while len(s) > 0:
-        l = len(s)
-        s = s.replace('()','')
-        s = s.replace('{}','')
-        s = s.replace('[]','')
-        if l==len(s): return False
-    return True
+                if ch == ")" and curr == "(":
+                    continue
+                elif ch == "]" and curr == "[":
+                    continue
+                elif ch == "}" and curr == "{":
+                    continue
+                else:
+                    return False
+        else:
+            return False
+            
+    return True if len(stack) == 0 else False
+                
 
 
-isValid("([{}])")
+
+# def isValid(s: str) -> bool:
+#     while len(s) > 0:
+#         l = len(s)
+#         s = s.replace('()','')
+#         s = s.replace('{}','')
+#         s = s.replace('[]','')
+#         if l==len(s): return False
+#     return True
+
+
+# isValid("([{}])")
