6dce32ccc170cfae897ed82ff39c5fd6ddbcf8aa
kevinren1108 <kevinren1108@gmail.com>
2022-04-08 17:50:14 -0600

    updateupdate

--- 37. Sudoku Solver/1.py
+++ 37. Sudoku Solver/1.py
@@ -1,19 +1,35 @@
 class Solution:
+    
     def solveSudoku(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-    def dfs(self, board, )    
+        self.dfs(board, 0,0)
         
-        
-        
+    def dfs(self, board, r, c):
+        while board[r][c] != ".":
+            c += 1
+            if c == 9:
+                r += 1
+                c = 0
+            if r == 9:
+                return True
+            
+        for i in range(1, 10):
+            tmp = str(i)
+            if self.valid(board, r, c, tmp):
+                board[r][c] = tmp
+                if self.dfs(board, r, c):
+                    return True
+        board[r][c] = "."
+        return False
         
     def valid(self, board, r, c, val):
-        for i in range(10):
+        for i in range(9):
             if board[i][c] == val:
                 return False
             
-        for i in range(10):
+        for i in range(9):
             if board[r][i] == val:
                 return False
             
